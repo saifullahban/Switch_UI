1a80099223a25d3b7ca56d7d0c562325952b66d3
saifullahban <saifbangash744@gmail.com>
2024-10-15 07:00:24 -0700

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import {View} from 'react-native';
+import React from 'react';
+import SwitchUI from './src/Component/SwitchUI';
+
+const App = () => {
+  return (
+    <View className="flex-1">
+      <SwitchUI/>
+    </View>
+  );
+};
+
+export default App;
